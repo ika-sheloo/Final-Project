3cc4353bbf5a6fac214011a7918127c60b8ca2ae
ika-sheloo <59067979+ika-sheloo@users.noreply.github.com>
2020-02-08 22:36:48 +0400

    final

---
+++ access/js/main.js
@@ -0,0 +1,12 @@
+  
+// let add = document.getElementsByTagName('svg').firstChild;
+// console.log(add);
+// let bl = add.length;
+// console.log(bl)
+// for (let i = 0; i < bl; i++) {
+	
+// 	add[i].addEventListener('click', function(e) {
+// 		console.log(i)
+// 		this.style.fill='black';
+// });
+// }
